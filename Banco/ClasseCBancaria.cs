c0a74b937938c3b5af558e4c94828308a25a0be0
DerickLFAndrade <derickspy15@gmail.com>
2021-11-12 23:40:15 -0300

    Adicionar arquivos de projeto.

---
+++ Banco/ClasseCBancaria.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco
+{
+    internal class ClasseCBancaria {
+  
+            public string Titular { get; set; }
+            public int Agencia { get; set; }
+            public int NumeroC { get; set; }
+            public double Saldo { get; set; }
+
+
+    }
+}
---
+++ Banco/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!\r\n Esse é meu primeiro programa em C#");
+
+            ClasseCBancaria novaConta = new ClasseCBancaria {
+
+                Titular = "Derick",
+                Agencia = 156,
+                NumeroC = 13345,
+                Saldo = 10000
+
+
+            };
+
+            Console.ReadKey();
+        }
+    }
+}
