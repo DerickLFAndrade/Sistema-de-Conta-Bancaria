b9eac5a605b5a6fbaea963e0aadef322df682378
Derick <Derick@WORKSTATION07>
2021-11-23 12:32:15 -0300

    Aprendendo a tratar erros com c#

---
+++ csharpexceptions-projetoInicial/ByteBank/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    class Program {
+        static void Main() {
+            M2();
+            Console.ReadLine();
+        }
+        public static void M2() {
+            try {
+                Dividir();
+            } catch (DivideByZeroException) {
+                Console.WriteLine("exceção tratada");
+            }
+        }
+
+       public static int Dividir(int n1 = 10, int n2 = 0) { 
+            return n1 / n2; 
+        }
+    }
+}
